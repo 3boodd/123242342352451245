48270535bc9855a0681a58067a25af919a0430ba
3boodd <36028899+3boodd@users.noreply.github.com>
2019-01-02 01:45:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,33 +38,27 @@ client.on("message", message => {
     });
 
 
+  client.on('message', message => {
+if(message.content.startsWith(prefix + "invite")) { 
+message.author.send(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2080374975`);
+}
+});
 
 
 client.on('message', message => {
+    if (!message.guild) return; 
     if (message.content.startsWith("رابط")) {
-        
-  message.channel.createInvite({
+
+        message.channel.createInvite({
         thing: true,
-        maxUses: 5,
+        maxUses: 100,
         maxAge: 86400
-    }).then(invite =>  
+    }).then(invite =>
       message.author.sendMessage(invite.url)
     )
-    const embed = new Discord.RichEmbed()
-        .setColor("2fff00")
-        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
-        .setFooter("Spring-Team")
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("2fff00")
-        .setDescription(`
-**-------------------
--هذا هو الرابط 
--ارسله للي تحب وحيآك انت وياه
--ونورنا ياجميل :heart: 
-------------------- **`)
-        .setFooter("By:shyboy_05")
-      message.author.sendEmbed(Embed11)
+  message.channel.send(`** تم ارسال الرابط في الخاص**`)
+
+      message.author.send(`**هذا الرابط صالح ل 100 شخص خلال 24 ساعة**`)
     }
 });
 
